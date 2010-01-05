ebf5bc355207c705e6521db4c85a39e567ffef8d
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2010-01-05 04:44:18 +0000

    Import of TOKUHIROM/Test-TCP-0.16 from CPAN.
    gitpan-cpan-distribution: Test-TCP
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         TOKUHIROM/Test-TCP-0.16.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- inc/YAML.pm
+++ inc/YAML.pm
@@ -1,13 +1,13 @@
 #line 1
 package YAML;
 
-use 5.008;
+use 5.008001;
 use strict;
 use warnings;
 use YAML::Base;
 use YAML::Node; # XXX This is a temp fix for Module::Build
 
-our $VERSION   = '0.70';
+our $VERSION   = '0.71';
 our @ISA       = 'YAML::Base';
 our @EXPORT    = qw{ Dump Load };
 our @EXPORT_OK = qw{ freeze thaw DumpFile LoadFile Bless Blessed };
@@ -58,7 +58,8 @@ sub DumpFile {
         }
         open $OUT, $mode, $filename
           or YAML::Base->die('YAML_DUMP_ERR_FILE_OUTPUT', $filename, $!);
-    }  
+    }
+    binmode $OUT, ':utf8';  # if $Config{useperlio} eq 'define';
     local $/ = "\n"; # reset special to "sane"
     print $OUT Dump(@_);
 }
@@ -70,9 +71,10 @@ sub LoadFile {
         $IN = $filename;
     }
     else {
-        open $IN, $filename
+        open $IN, '<', $filename
           or YAML::Base->die('YAML_LOAD_ERR_FILE_INPUT', $filename, $!);
     }
+    binmode $IN, ':utf8';  # if $Config{useperlio} eq 'define';
     return Load(do { local $/; <$IN> });
 }
 
@@ -105,4 +107,4 @@ __END__
 
 =encoding utf8
 
-#line 815
+#line 817
